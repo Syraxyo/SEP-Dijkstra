9899076a31bc3905309ea20f452a9a4c6ced63b7
Syraxyo <38350315+Syraxyo@users.noreply.github.com>
2018-04-13 15:48:31 +0200

    Add files via upload

---
+++ testSep.java
@@ -0,0 +1,15 @@
+
+public class testSep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 5;
+		
+		int a = 10;
+		
+		System.out.println(a+i);
+		
+	}
+
+}
